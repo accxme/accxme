315acbe950af622f92830b7df656c133aef1cfda
32Islam <oislam1144@gmail.com>
2022-03-24 12:22:53 +0400

    fixes

--- Website/js/datav1023212.js
+++ Website/js/datav1023212.js
@@ -70,7 +70,7 @@
 })()
 
  // Set the date we're counting down to
- var countDownDate = new Date("Feb 10, 2022 21:30:00").getTime();
+ var countDownDate = new Date("MAR 30, 2022 20:30:00").getTime();
         
  // Update the count down every 1 second
  var x = setInterval(function() {
@@ -88,13 +88,13 @@
    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
  
    // Display the result in the element with id="demo"
-   document.getElementById("timer").innerHTML = "ARAB SCRIMS ONE LOBBY CUSTOM C3S2 | POWERED BY ACCXME | STARTING IN " + days + "d " + hours + "h "
+   document.getElementById("timer").innerHTML = "R3N4D x XDI_A 200$ DUOS CUP OPENS | POWERED BY ACCXME | STARTING IN " + days + "d " + hours + "h "
    + minutes + "m " + seconds + "s ";
  
    // If the count down is finished, write some text
    if (distance < 0) {
      clearInterval(x);
-     document.getElementById("timer").innerHTML = "ARAB SCRIMS ONE LOBBY CUSTOM C3S2 | POWERED BY ACCXME | SCORINGS ARE SYNCED WITH WARLEGEND" ;
+     document.getElementById("timer").innerHTML = "R3N4D x XDI_A 200$ DUOS CUP OPENS | POWERED BY ACCXME | SCORINGS ARE SYNCED WITH WARLEGEND" ;
    }
  }, 1000);
  function tableSearch() {
